cfb3a5229471ccfc37d0088355554e20d9d44887
RomanMukha <Quarian@mail.ru>
2020-08-28 23:16:44 +0300

    Автоматизация на Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,46 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+const browserSync = require("browser-sync");
+
+// clean
+
+const clean = () => {
+  return del("build");
+};
+
+exports.clean = clean;
+
+// Copy
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
+
+// HTML copy
+
+const html = () => {
+  return gulp.src("source/*.html", {base: "source"})
+  .pipe(gulp.dest("build"));
+};
+
+exports.html = html;
 
 // Styles
 
@@ -16,26 +56,63 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
-}
+};
 
 exports.styles = styles;
+
+//images
+
+const images = () => {
+  return gulp.src("build/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel:3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo({plugins: [{removeViewBox: false}]})
+    ]))
+    .pipe(gulp.dest("build/img"))
+};
+
+exports.images = images;
+
+//WebP images
+
+const createWebp = () => {
+  return gulp.src("build/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
+};
+
+exports.webp = createWebp;
+
+//Sprite
+
+const sprite = () => {
+  return gulp.src(["build/img/**/icon-*.svg", "build/img/**/logo-htmlacademy.svg"])
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+};
+
+exports.sprite = sprite;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
+};
 
 exports.server = server;
 
@@ -43,9 +120,14 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/*.html").on("change", gulp.series("html"));
   gulp.watch("source/*.html").on("change", sync.reload);
 }
 
+exports.build = gulp.series(
+  clean, copy, html, images, sprite, styles
+);
+
 exports.default = gulp.series(
-  styles, server, watcher
+  clean, copy, html, images, sprite, styles, server, watcher
 );
